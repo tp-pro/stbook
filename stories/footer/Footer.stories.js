415084417c49bb11a6ebec552c9a797d3e6e8088
tp-pro <tpasquier.pro@gmail.com>
2023-11-16 21:44:53 +0100

    footer stories and structure

--- stories/footer/Footer.stories.js
+++ stories/footer/Footer.stories.js
@@ -5,24 +5,27 @@ export default {
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/html/writing-docs/autodocs
   tags: ['autodocs'],
   render: (args) => createFooter(args),
-  parameters: {
-    // More on how to position stories at: https://storybook.js.org/docs/html/configure/story-layout
-    layout: 'fullscreen',
-  },
-  // More on argTypes: https://storybook.js.org/docs/html/api/argtypes
-  argTypes: {
-    onLogin: { action: 'onLogin' },
-    onLogout: { action: 'onLogout' },
-    onCreateAccount: { action: 'onCreateAccount' },
-  },
-};
 
-export const LoggedIn = {
-  args: {
-    user: {
-      name: 'Jane Doe',
+  argTypes: {
+    backgroundColor: { control: 'color' },
+    primary: { control: 'boolean' },
+    size: {
+      control: { type: 'select' },
+      options: ['full', 'medium'],
     },
   },
 };
 
-export const LoggedOut = {};
+export const Full = {
+    args: {
+      size: 'full',
+    },
+  };
+  
+  export const Medium = {
+    args: {
+      size: 'medium',
+    },
+  };
+
+
